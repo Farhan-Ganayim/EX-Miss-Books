2aa100a196717bbc60d00ec82e253adb24087129
Farhan-Ganayim <f.ganayim@gmail.com>
2025-02-09 14:22:59 +0200

    Add basic layout style to header & book list

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,15 +1,15 @@
 export function AppHeader({ onSetPage }) {
 return (
-    <header className="app-header full main-layout">
-        <section>
-            <h1>Miss Book</h1>
-            <nav className="app-nav">
+    <header className="app-header flex align-center justify-between main-layout">
+        {/* <section className=""> */}
+            <h1 className="header-logo">Miss Book</h1>
+            <nav className="app-nav flex">
                 <a onClick={()=>onSetPage('home')} href="#">Home</a>
                 <a onClick={()=>onSetPage('books')} href="#">Books</a>
                 <a onClick={()=>onSetPage('about')} href="#">About</a>
 
             </nav>
-        </section>
+        {/* </section> */}
 
     </header>
 )
--- cmps/BookList.jsx
+++ cmps/BookList.jsx
@@ -3,7 +3,7 @@ import { BookPreview } from "./BookPreview.jsx"
 export function BookList( {books} ) {
 
     return (
-        <section className="book-list-container">
+        <section className="book-list-container flex justify-center">
                 {books.map(book =>
                     <div key={book.id} className="book-card">
                         <BookPreview book={book}/>
